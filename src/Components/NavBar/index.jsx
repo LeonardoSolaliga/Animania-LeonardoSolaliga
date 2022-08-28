f902b83e3ad1dc47c579c9420ce4d81d0a4b4e0b
unknown <leo_nosecuanto@hotmail.com>
2022-08-28 20:05:03 -0300

    agregar navbar y titulo

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./styles.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div class="flexeo">
+        <a class="letra" href=" ">
+          LogoAnimania
+        </a>
+        <nav>
+          <ul class="navlinks">
+            <li>
+              <a class="letra" href=" ">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a class="letra" href=" ">
+                Manga
+              </a>
+            </li>
+            <li>
+              <a class="letra" href=" ">
+                Juegos
+              </a>
+            </li>
+            <li>
+              <a class="letra" href=" ">
+                Figuras
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="encerrador">
+        <h1>Animania</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
